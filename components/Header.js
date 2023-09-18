334c6ee2b8aac1c84ad5addc4813b08b852f2b84
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-18 14:26:57 +0500

    unistalled mui from the app because itwas not working fine with nextjs

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,42 @@
+'use client'
+
+// import { AppBar, Toolbar, Typography, CssBaseline } from "@mui/material"
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import CssBaseline from "@mui/material/CssBaseline"
+
+// import { makeStyles } from '@mui/styles'
+
+
+// const useStyles = makeStyles((theme) => ({
+//     appBar: {
+//         borderBottom: `1px solid ${theme.palette}`,
+//     }
+// }))
 
 const Header = () => {
+    // const classes = useStyles();
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <CssBaseline />
+
+            <AppBar
+                color="white"
+                position="static"
+                elevation={0}
+            // className={classes.appBar}
+            >
+
+                <Toolbar>
+
+                    <Typography variant="h6" color='inherit' noWrap></Typography>
+
+                </Toolbar>
+
+            </AppBar>
+
+        </>
     )
 }
 
