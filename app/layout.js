97e98be50ec59abf5eadaf46cbd6c4984ccffd17
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-19 23:14:27 +0500

    created a route for blogs post and a nested routes for blog details, within these routes i have created a layout for the blogs routes that i am going to render on every blog page

--- app/layout.js
+++ app/layout.js
@@ -15,16 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <div className='flex flex-row w-full h-full relative'>
-          <div className='w-60 h-full hidden lg:flex flex-col fixed top-16 left-0'>
-            <Sidebar />
-          </div>
-          <div className='w-full lg:w-[calc(100%-15rem)] overflow-y-auto overflow-x-hidden absolute top-16 right-0'>
-            {children}
-            <Footer />
-          </div>
-        </div>
+        {children}
       </body>
     </html>
   )
