ec03e1e8696a7e8fca46b30f674d16bf68285826
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-23 20:33:48 +0500

    created a landing page of the website

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
-import Header from '@components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@components/Footer'
-import Sidebar from '@components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         {children}
       </body>
