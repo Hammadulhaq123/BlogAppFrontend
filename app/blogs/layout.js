2197f780728b40aa6257aee8a854aebf4e54d83a
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-24 20:24:30 +0500

    updated the layout after noticing an error in the console. Inn the layout file i was re-rendering the html and body tag. Corrected the mistake by simply removing the html and body tag from the layout as it was already rendered throught the global layout.js

--- app/blogs/layout.js
+++ app/blogs/layout.js
@@ -4,23 +4,24 @@ import { Inter } from 'next/font/google'
 import Sidebar from '@components/Sidebar'
 const inter = Inter({ subsets: ['latin'] })
 
-
+export const metadata = {
+    title: 'Blogs',
+    description: 'Present your creative writing like a social media post.',
+}
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-
-            <body className={inter.className}>
-                <Header />
-                <div className='flex flex-row w-full h-full relative'>
-                    <div className='w-60 hidden lg:flex flex-col fixed top-16 left-0'>
-                        <Sidebar />
-                    </div>
-                    <div className='w-full lg:w-[calc(100%-15rem)] overflow-y-auto overflow-x-hidden absolute top-16 right-0'>
-                        {children}
-                    </div>
+        <>
+            <Header />
+            <div className='flex flex-row w-full h-full relative'>
+                <div className='w-60 hidden lg:flex flex-col fixed top-16 left-0'>
+                    <Sidebar />
                 </div>
-            </body>
-        </html>
+                <div className='w-full lg:w-[calc(100%-15rem)] overflow-y-auto overflow-x-hidden absolute top-16 right-0'>
+                    {children}
+                </div>
+            </div>
+        </>
+
     )
 }
