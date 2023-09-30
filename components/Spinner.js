93be294d08c375883d3a028b2f8d378a7388cd39
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-30 16:29:33 +0500

    Created an spinner component to show a loading state of the detailed blog post page

---
+++ components/Spinner.js
@@ -0,0 +1,12 @@
+
+
+const Spinner = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <svg width="40" height="40" fill="#2f80ed" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><defs><filter id="spinner-gF00"><feGaussianBlur in="SourceGraphic" stdDeviation="1.5" result="y"></feGaussianBlur><feColorMatrix in="y" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7" result="z"></feColorMatrix><feBlend in="SourceGraphic" in2="z"></feBlend></filter></defs><g filter="url(#spinner-gF00)"><circle cx="4" cy="12" r="3"><animate attributeName="cx" calcMode="spline" dur="0.75s" values="4;9;4" keySplines=".56,.52,.17,.98;.56,.52,.17,.98" repeatCount="indefinite"></animate><animate attributeName="r" calcMode="spline" dur="0.75s" values="3;8;3" keySplines=".56,.52,.17,.98;.56,.52,.17,.98" repeatCount="indefinite"></animate></circle><circle cx="15" cy="12" r="8"><animate attributeName="cx" calcMode="spline" dur="0.75s" values="15;20;15" keySplines=".56,.52,.17,.98;.56,.52,.17,.98" repeatCount="indefinite"></animate><animate attributeName="r" calcMode="spline" dur="0.75s" values="8;3;8" keySplines=".56,.52,.17,.98;.56,.52,.17,.98" repeatCount="indefinite"></animate></circle></g></svg>
+
+        </div>
+    )
+}
+
+export default Spinner
